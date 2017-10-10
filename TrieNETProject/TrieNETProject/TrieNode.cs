1d322f914ecc45f061f5fbb59491abf73ff1ed28
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-10 13:26:42 +0500

    TrieNode to represent a node in Trie

---
+++ TrieNETProject/TrieNETProject/TrieNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieNETProject
+{
+    public class TrieNode
+    {
+        public bool EndOfWord { get; set; }
+        public Dictionary<char, TrieNode> Children { get; set; }
+
+        public TrieNode()
+        {
+            Children = new Dictionary<char, TrieNode>();
+        }
+
+        public TrieNode(bool isLast)
+        {
+            Children = new Dictionary<char, TrieNode>();
+            EndOfWord = isLast;
+        }
+
+        public void Add(string w, TrieNode root)
+        {
+            if(string.IsNullOrEmpty(w))
+                return;
+            TrieNode curr = root;
+            for (int i = 0; i < w.Length - 1; i++)
+            {
+                var node = AddCh(w[i], false, curr);
+                curr = node;
+            }
+            //add last char
+            AddCh(w.Last(), true, curr);
+        }
+
+        private TrieNode AddCh(char ch, bool isLast, TrieNode curr)
+        {
+            if (curr.Children.ContainsKey(ch))
+                return curr.Children[ch];
+            var trieNode = new TrieNode(isLast);
+            curr.Children.Add(ch, trieNode);
+            return trieNode;
+        }
+    }
+}
