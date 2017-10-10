c02bca856a9dc3c1ca00b14ffe4d8e317c540c02
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-10 18:57:26 +0500

    all classes are moved to TrieNET class library, test infrastructure is created

--- TrieNETProject/TrieNETProject/Program.cs
+++ TrieNETProject/TrieNETProject/Program.cs
@@ -10,6 +10,20 @@ namespace TrieNETProject
     {
         static void Main(string[] args)
         {
+            var root = new TrieNode();
+            var trie = new Trie(root);
+            trie.Add("abc");
+            trie.Add("abgl");
+            trie.Add("cdf");
+            trie.Add("abcd");
+            trie.Add("lmn");
+
+            var contains = trie.ContainsPrefix("cdf");
+            var contains1 = trie.ContainsPrefix("cd");
+            var contains2 = trie.ContainsPrefix("df");
+            var contains3 = trie.ContainsPrefix("ml");
+            var res = new List<bool>() {contains,contains1,contains2,contains3};
+            
         }
     }
 }
