b9e5ac7128e0bab1500fcc844d37212814d52e0d
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-10 13:46:27 +0500

    ITrie interface + Trie implementation class - template, not implemented

---
+++ TrieNETProject/TrieNETProject/ITrie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieNETProject
+{
+    interface ITrie
+    {
+        void Add(string word);
+        bool ContainsWord(string word);
+        bool ContainsPrefix(string prefix);
+    }
+}
---
+++ TrieNETProject/TrieNETProject/Trie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrieNETProject
+{
+    public class Trie : ITrie
+    {
+        public TrieNode Root { get; private set; }
+
+        public Trie(TrieNode root)
+        {
+            Root = root;
+        }
+
+        public void Add(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsPrefix(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
