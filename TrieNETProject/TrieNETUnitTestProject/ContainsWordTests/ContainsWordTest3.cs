6dc5578478b7902e94613169f64ea77879335f20
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-11 22:39:27 +0500

    Tests
    ContainsWord tests

---
+++ TrieNETProject/TrieNETUnitTestProject/ContainsWordTests/ContainsWordTest3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrieNETProject;
+
+namespace TrieNETUnitTestProject
+{
+    [TestClass]
+    public class ContainsWordTest3
+    {
+        private Trie ActualTrie;
+
+        [TestInitialize]
+        public void Init()
+        {
+            ActualTrie = TestTries.Get(2);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("a"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("ab"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("abcd"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("abc"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("ak"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("t"));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("12345"));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("abgl"));
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("cdf"));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("lmn"));
+        }
+    }
+}
