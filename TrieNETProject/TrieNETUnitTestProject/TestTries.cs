838562a85d4aacdc6bd66d7c1ac8443973d963fe
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-11 17:07:19 +0500

    Tests
    More Test Tries

--- TrieNETProject/TrieNETUnitTestProject/TestTries.cs
+++ TrieNETProject/TrieNETUnitTestProject/TestTries.cs
@@ -26,6 +26,7 @@ namespace TrieNETUnitTestProject
         {
             Instances.Add(Create1());
             Instances.Add(Create2());
+            Instances.Add(Create3());
         }
 
         private static Trie Create1()//"abc"
@@ -58,16 +59,36 @@ namespace TrieNETUnitTestProject
 
         private static Trie Create3()//"abc","abgl","cd","abcd", "lmn"
         {
-            throw new NotImplementedException();
-            //var root = new TrieNode();
-            //var bTrieNode = new TrieNode();
-            //var cTrieNode = new TrieNode();
-            //var endTrieNode = new TrieNode(true);
-            //root.Children.Add('a', bTrieNode);
-            //bTrieNode.Children.Add('b', cTrieNode);
-            //cTrieNode.Children.Add('c', endTrieNode);
+            var root = new TrieNode();
+            var bTrieNode = new TrieNode();
+            var lTrieNode = new TrieNode();
+            var d1TrieNode = new TrieNode();
+            var d2TrieNode = new TrieNode();
+            var mTrieNode = new TrieNode();
+            var nTrieNode = new TrieNode();
+            var cgTrieNode = new TrieNode();
+            var fTrieNode = new TrieNode();
+            var endTrieNode1 = new TrieNode(true);
+            var endTrieNode2 = new TrieNode(true);
+            var endTrieNode3 = new TrieNode(true);
+            var endTrieNode4 = new TrieNode(true);
 
-            //return new Trie(root);
+            root.Children.Add('a', bTrieNode);
+            root.Children.Add('c', d1TrieNode);
+            root.Children.Add('l', mTrieNode);
+            bTrieNode.Children.Add('b', cgTrieNode);
+            cgTrieNode.Children.Add('c', d2TrieNode);
+            cgTrieNode.Children.Add('g', lTrieNode);
+            lTrieNode.Children.Add('l', endTrieNode2);
+            d2TrieNode.Children.Add('d', endTrieNode1);
+
+            d1TrieNode.Children.Add('d', fTrieNode);
+            fTrieNode.Children.Add('f', endTrieNode3);
+
+            mTrieNode.Children.Add('m', nTrieNode);
+            nTrieNode.Children.Add('n', endTrieNode4);
+
+            return new Trie(root);
         }
     }
 }
