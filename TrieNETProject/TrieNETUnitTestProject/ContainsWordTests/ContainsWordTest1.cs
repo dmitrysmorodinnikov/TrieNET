8dd383aef5745fb2922ab2d8e8fd7ce99c4b655a
d_smorodinnikov <smorodinnikov@mail.ru>
2017-10-11 21:57:14 +0500

    Tests
    ContainsWordTest1

---
+++ TrieNETProject/TrieNETUnitTestProject/ContainsWordTests/ContainsWordTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrieNETProject;
+
+namespace TrieNETUnitTestProject
+{
+    [TestClass]
+    public class ContainsWordTest1
+    {
+        private Trie ActualTrie;
+
+        [TestInitialize]
+        public void Init()
+        {
+            ActualTrie = TestTries.Get(0);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("a"));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("ab"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(ActualTrie.ContainsWord("abc"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("abcd"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("ak"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("t"));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.IsFalse(ActualTrie.ContainsWord("12345"));
+        }
+    }
+}
